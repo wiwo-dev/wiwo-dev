13bb60d7924b328b77ad80c2c71637297aaa7751
wiwo-one <wojtekwieclawski@gmail.com>
2022-04-29 15:12:40 +0200

    click

--- src/components/WindowCard.js
+++ src/components/WindowCard.js
@@ -3,7 +3,11 @@ import { Badge, Box, Center, Heading, HStack, Text, VStack, Link } from "@chakra
 export default function WindowCard({ children }) {
   return (
     <>
-      <VStack spacing="0" shadow="16px 16px 0px #C4C4C4">
+      <VStack
+        spacing="0"
+        shadow="16px 16px 0px #C4C4C4"
+        _hover={{ shadow: "20px 20px 0px #C4C4C4", transform: "translate(-2px, -2px)" }}
+        _active={{ shadow: "8px 8px 0px #C4C4C4", transform: "translate(10px, 10px)" }}>
         <Box
           w="full"
           h="33"
@@ -19,7 +23,14 @@ export default function WindowCard({ children }) {
           </HStack>
           <Box borderLeft="4px" borderColor="black" h="100%" w="33px"></Box>
         </Box>
-        <Box w="100%" minH="50px" padding="36px" borderWidth="0px 4px 4px 4px" borderColor="black">
+        <Box
+          w="100%"
+          minH="50px"
+          paddingX="36px"
+          paddingTop="52px"
+          paddingBottom="62px"
+          borderWidth="0px 4px 4px 4px"
+          borderColor="black">
           {children}
         </Box>
       </VStack>
